0bbda6c2409b8b30b1b979aee7b3982af2244a9a
inFngNam <inf.ng.nam@gmail.com>
2022-01-04 17:17:55 +0700

    get unreaded mail

---
+++ python/get_unreaded_mail.py
@@ -0,0 +1,36 @@
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def main():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+            # Save the credentials for the next ruin
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        result= service.users().messages().list(userId='me', q="in:inbox is:unread").execute()        
+        print(result['resultSizeEstimate'])
+    except HttpError as error:
+        print(0)
+
+
+if __name__ == '__main__':
+    main()
+# [END gmail_quickstart]
