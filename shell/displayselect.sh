cfae8359a0f44dd3b0530ebed0e5a832037e95a6
inFngNam <inf.ng.nam@gmail.com>
2022-01-09 18:06:15 +0700

    Update scripts

--- shell/displayselect.sh
+++ shell/displayselect.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
-intern="eDP1"
-VGA="VGA1"
-HDMI="HDMI1"
 
-choices="VGA\nHDMI\nLaptop"
-
+connected_display=$(xrandr | grep -w 'connected' | awk '{print $1}')
+choices="$connected_display"
 chosen=$(echo -e "$choices"| dmenu -i -p "Select display" -fn "Hack:size=13:antilias=true:autohint=true" -nb "#222222" -nf "#444444" -sb "#f9906f" -sf "#eeeeee")
 
+VGA="$(echo "$connected_display" | grep 'VGA')"
+intern="$(echo "$connected_display" | grep 'DP')"
+HDMI="$(echo "$connected_display" | grep 'HDMI')"
+
 case "$chosen"  in
-	VGA)
-		if xrandr | grep "VGA" | grep -w "connected"; then
-			xrandr --output "$HDMI" --off --output "$intern" --off --output "$VGA" --primary --auto
-		else 
-			xrandr --ouput "$VGA" --off --output "$HDMI" --off --output "$intern" --primary --auto
-		fi
-		;;
-	HDMI)
-		if xrandr | grep "HDMI" | grep -w "connected"; then 
-			xrandr --ouput "$VGA" --off --output "$intern" --off --output "$HDMI" --primary --auto
+    "$VGA")
+    	if [[ "$VGA" != "" ]]; then
+            xrandr --output "$VGA" --primary --auto --output "$HDMI" --off --output "$intern" --off
+    	else 
+    	    xrandr --output "$VGA" --off --output "$HDMI" --off --output "$intern" --primary --auto
+    	fi
+    	;;
+    "$HDMI")
+		if [[ "$HDMI" != "" ]]; then 
+		    xrandr --output "$VGA" --off --output "$intern" --off --output "$HDMI" --primary --auto
 		else
 			xrandr --output "$VGA" --off --output "$HDMI" --off --output "$intern" --primary --auto
 		fi
 		;;
-	Laptop)
+	"$intern")
 		xrandr --output "$VGA" --off --output "$HDMI" --off --output "$intern" --primary --auto
 		;;
 esac
